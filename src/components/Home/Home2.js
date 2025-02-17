eaa0c06fd4f0dde4a97d616dccb5bf737930354a
Vtormacs <macskids98play@hotmail.com>
2025-02-17 15:30:23 -0300

    alteracao de avatar

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import myImg from "../../Assets/avatar2.jpeg";
+import myImg from "../../Assets/avatar3.jpeg";
 import Tilt from "react-parallax-tilt";
 import { AiFillGithub, AiFillInstagram } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
